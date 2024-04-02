28e92ca22becafa8f39c1b6c618c3ffb62145d02
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2024-04-01 18:08:29 -0700

    Added Readme code samples

---
+++ Assets/Samples/ImageLoaderSample.cs
@@ -0,0 +1,19 @@
+﻿using Extensions.Unity.ImageLoader;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageLoaderSample : MonoBehaviour
+{
+    [SerializeField] string imageURL;
+    [SerializeField] Image image;
+
+    async void Start()
+    {
+        // Loading sprite from web, cached for quick load next time
+        image.sprite = await ImageLoader.LoadSprite(imageURL);
+
+        // Same loading with auto set to image
+        await ImageLoader.SetSprite(imageURL, image);
+    }
+}
