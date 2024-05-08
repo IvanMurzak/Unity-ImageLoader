6e232601362b80f8c6829d51464f63688f343d0c
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2024-05-08 02:17:43 -0700

    Refactoring

--- Assets/_PackageRoot/Runtime/Future/Future.cs
+++ Assets/_PackageRoot/Runtime/Future/Future.cs
@@ -41,6 +41,19 @@ namespace Extensions.Unity.ImageLoader
             cancellationToken.Register(Cancel);
         }
         ~Future() => Dispose();
+        
+        internal Future<T> PassEvents(Future<T> to)
+        {
+            OnLoadedFromMemoryCache += (v) => to.Loaded(v, FutureLoadedFrom.MemoryCache);
+            OnLoadingFromDiskCache  += ( ) => to.Loading(FutureLoadingFrom.DiskCache);
+            OnLoadedFromDiskCache   += (v) => to.Loaded(v, FutureLoadedFrom.DiskCache);
+            OnLoadingFromSource     += ( ) => to.Loading(FutureLoadingFrom.Source);
+            OnLoadedFromSource      += (v) => to.Loaded(v, FutureLoadedFrom.Source);
+            OnFailedToLoad          += to.FailToLoad;
+            OnCancelled             += to.Cancel;
+
+            return this;
+        }
         internal void Loading(FutureLoadingFrom loadingFrom)
         {
             if (cleared || IsCancelled) return;
