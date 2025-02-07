fab46dd8e6a027a5b77ffa697535912c9b9c4f71
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-02-07 01:11:06 -0800

    Added methods to configure disk and memory cache usage in Future class

---
+++ Assets/_PackageRoot/Runtime/Future/Future.API.Set.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Extensions.Unity.ImageLoader
+{
+    public partial class Future<T> : IDisposable
+    {
+        /// <summary>
+        /// Set disk cache usage for this Future instance
+        /// </summary>
+        /// <param name="value">new value</param>
+        /// <returns>Returns the Future instance</returns>
+        public Future<T> SetUseDiskCache(bool value = true)
+        {
+            UseDiskCache = value;
+            return this;
+        }
+
+        /// <summary>
+        /// Set memory cache usage for this Future instance
+        /// </summary>
+        /// <param name="value">new value</param>
+        /// <returns>Returns the Future instance</returns>
+        public Future<T> SetUseMemoryCache(bool value = true)
+        {
+            UseMemoryCache = value;
+            return this;
+        }
+    }
+}
