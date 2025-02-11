e5eb58ce65c76e0ac01ed748c567d7acc48e935a
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-02-11 01:37:08 -0800

    Add GetValueOrDefault extension method for Dictionary

--- Assets/_PackageRoot/Runtime/Extensions.cs
+++ Assets/_PackageRoot/Runtime/Extensions.cs
@@ -19,5 +19,9 @@ namespace Extensions.Unity.ImageLoader
                 }
             }
         }
+        public static TValue GetValueOrDefault<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey key, TValue defaultValue = default)
+        {
+            return dictionary.TryGetValue(key, out var value) ? value : defaultValue;
+        }
     }
 }
