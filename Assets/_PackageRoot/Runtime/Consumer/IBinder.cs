baeae72f2d0d60cb2e2310ff2021b54351862e2f
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-03-05 20:46:30 -0800

    Deleted unused binder class

--- Assets/_PackageRoot/Runtime/Consumer/IBinder.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Extensions.Unity.ImageLoader
-{
-    public interface IBinder<Target, Value> : IDisposable
-    {
-        void Set(Target target, Value value, DebugLevel logLevel = DebugLevel.Error);
-    }
-}
