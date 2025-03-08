71f2492952faca1bc6a11023436cacce48bb889b
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-03-07 23:36:54 -0800

    Added SetPlaceholder functions for all input types

---
+++ Assets/_PackageRoot/Runtime/Future/Extensions/FutureEx.Placeholder.Color.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Concurrent;
+using UnityEngine;
+
+namespace Extensions.Unity.ImageLoader
+{
+    public static partial class FutureEx
+    {
+        /// <summary>
+        /// Set a placeholder in all conditions for this Future instance
+        /// </summary>
+        /// <param name="color">new placeholder</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Texture2D> SetPlaceholder(this IFuture<Texture2D> future, Color color) => future.SetPlaceholder(color,
+            PlaceholderTrigger.LoadingFromDiskCache,
+            PlaceholderTrigger.LoadingFromSource,
+            PlaceholderTrigger.FailedToLoad,
+            PlaceholderTrigger.Canceled);
+
+        /// <summary>
+        /// Set a placeholder in specified conditions for this Future instance
+        /// </summary>
+        /// <param name="color">hex color to fill solid color</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Texture2D> SetPlaceholder(this IFuture<Texture2D> future, Color color, params PlaceholderTrigger[] triggers)
+            => future.SetPlaceholder(ColorUtility.ToHtmlStringRGBA(color), triggers);
+
+        /// <summary>
+        /// Set a placeholder in all conditions for this Future instance
+        /// </summary>
+        /// <param name="color">new placeholder</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Sprite> SetPlaceholder(this IFuture<Sprite> future, Color color) => future.SetPlaceholder(color,
+            PlaceholderTrigger.LoadingFromDiskCache,
+            PlaceholderTrigger.LoadingFromSource,
+            PlaceholderTrigger.FailedToLoad,
+            PlaceholderTrigger.Canceled);
+
+        /// <summary>
+        /// Set a placeholder in specified conditions for this Future instance
+        /// </summary>
+        /// <param name="hexColor">hex color to fill solid color</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Sprite> SetPlaceholder(this IFuture<Sprite> future, Color color, params PlaceholderTrigger[] triggers)
+            => future.SetPlaceholder(ColorUtility.ToHtmlStringRGBA(color), triggers);
+
+        /// <summary>
+        /// Set a placeholder in all conditions for this Future instance
+        /// </summary>
+        /// <param name="color">new placeholder</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Reference<Texture2D>> SetPlaceholder(this IFuture<Reference<Texture2D>> future, Color color) => future.SetPlaceholder(color,
+            PlaceholderTrigger.LoadingFromDiskCache,
+            PlaceholderTrigger.LoadingFromSource,
+            PlaceholderTrigger.FailedToLoad,
+            PlaceholderTrigger.Canceled);
+
+        /// <summary>
+        /// Set a placeholder in specified conditions for this Future instance
+        /// </summary>
+        /// <param name="hexColor">hex color to fill solid color</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Reference<Texture2D>> SetPlaceholder(this IFuture<Reference<Texture2D>> future, Color color, params PlaceholderTrigger[] triggers)
+            => future.SetPlaceholder(ColorUtility.ToHtmlStringRGBA(color), triggers);
+
+        /// <summary>
+        /// Set a placeholder in all conditions for this Future instance
+        /// </summary>
+        /// <param name="color">new placeholder</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Reference<Sprite>> SetPlaceholder(this IFuture<Reference<Sprite>> future, Color color) => future.SetPlaceholder(color,
+            PlaceholderTrigger.LoadingFromDiskCache,
+            PlaceholderTrigger.LoadingFromSource,
+            PlaceholderTrigger.FailedToLoad,
+            PlaceholderTrigger.Canceled);
+
+        /// <summary>
+        /// Set a placeholder in specified conditions for this Future instance
+        /// </summary>
+        /// <param name="hexColor">hex color to fill solid color</param>
+        /// <returns>Returns the Future instance</returns>
+        public static IFuture<Reference<Sprite>> SetPlaceholder(this IFuture<Reference<Sprite>> future, Color color, params PlaceholderTrigger[] triggers)
+            => future.SetPlaceholder(ColorUtility.ToHtmlStringRGBA(color), triggers);
+    }
+}
--- Assets/_PackageRoot/Runtime/Future/Extensions/FutureEx.Placeholder.cs
+++ Assets/_PackageRoot/Runtime/Future/Extensions/FutureEx.Placeholder.cs
@@ -1,10 +1,23 @@
-﻿using UnityEditor.PackageManager;
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
 using UnityEngine;
 
 namespace Extensions.Unity.ImageLoader
 {
     public static partial class FutureEx
     {
+        const int PlaceholderSolidColorTextureSize = 2;
+        static ConcurrentDictionary<Type, ConcurrentDictionary<string, object>> colors = new ConcurrentDictionary<Type, ConcurrentDictionary<string, object>>();
+
+        public static Texture2D CreateFillTexture(this Color color, int width = PlaceholderSolidColorTextureSize, int height = PlaceholderSolidColorTextureSize)
+        {
+            var texture = new Texture2D(width, height);
+            texture.SetPixels(Enumerable.Repeat(color, width * height).ToArray());
+            texture.Apply();
+            return texture;
+        }
+
         /// <summary>
         /// Set a placeholder in all conditions for this Future instance
         /// </summary>
