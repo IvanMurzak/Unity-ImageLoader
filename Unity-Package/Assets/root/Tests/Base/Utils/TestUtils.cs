2641675b7bce051cb135389e6525273d923061cd
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-09-24 00:49:26 -0700

    Update image URLs in TestUtils and add test images

--- Unity-Package/Assets/root/Tests/Base/Utils/TestUtils.cs
+++ Unity-Package/Assets/root/Tests/Base/Utils/TestUtils.cs
@@ -12,9 +12,9 @@ namespace Extensions.Unity.ImageLoader.Tests.Utils
     {
         public static readonly string[] ImageURLs =
         {
-            "https://github.com/IvanMurzak/Unity-ImageLoader/raw/main/Test%20Images/ImageA.jpg",
-            "https://github.com/IvanMurzak/Unity-ImageLoader/raw/main/Test%20Images/ImageB.png",
-            "https://github.com/IvanMurzak/Unity-ImageLoader/raw/main/Test%20Images/ImageC.png"
+            "https://github.com/IvanMurzak/Unity-ImageLoader/raw/main/docs/img/test/ImageA.jpg",
+            "https://github.com/IvanMurzak/Unity-ImageLoader/raw/main/docs/img/test/ImageB.png",
+            "https://github.com/IvanMurzak/Unity-ImageLoader/raw/main/docs/img/test/ImageC.png"
         };
         public static string IncorrectImageURL => $"https://doesntexist.com/{Guid.NewGuid()}.png";
         public static IEnumerable<string> IncorrectImageURLs(int count = 3) => Enumerable.Range(0, count).Select(_ => IncorrectImageURL);
